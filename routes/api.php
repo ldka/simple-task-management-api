4537d155196c1a8e0a5c6cf78bf2e9bbc7b50b3a
ldka <49382149+ldka@users.noreply.github.com>
2025-04-21 04:34:36 +0800

    task patch request revert
    fixed nginx issue on query params

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/tasks/{task}', [TaskController::class, "update"]);
+    // Route::post('/tasks/{task}', [TaskController::class, "update"]);
     Route::apiResource('/tasks', TaskController::class);
 });
